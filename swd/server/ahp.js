0bfee327695ccf38fbf8536d19b66733f51285d1
olavv09 <w.ola0904@gmail.com>
2023-12-10 23:48:09 +0100

    poprawione obliczenia

--- swd/server/ahp.js
+++ swd/server/ahp.js
@@ -19,6 +19,7 @@ function processMatrices(req, res) {
   );
   const response = [calculateWeights(matrixPrice), calculateWeights(matrixAttractions), calculateWeights(matrixWeather)]
   res.json(JSON.stringify(response))
+  // res.json(JSON.stringify(calculateWeights(matrixPrice)))
 }
 
 function createMatrix(priceWS, priceWH, priceSH) {
@@ -27,19 +28,19 @@ function createMatrix(priceWS, priceWH, priceSH) {
   matrix[0][1] = convertPrice(priceWS);
   matrix[0][2] = convertPrice(priceWH);
 
-  matrix[1][0] = convertPrice(priceWS);
+  matrix[1][0] = 1 / convertPrice(priceWS);
   matrix[1][2] = convertPrice(priceSH);
 
-  matrix[2][0] = convertPrice(priceWH);
-  matrix[2][1] = convertPrice(priceSH);
+  matrix[2][0] = 1 / convertPrice(priceWH);
+  matrix[2][1] = 1 / convertPrice(priceSH);
 
   return normalizeMatrix(matrix);
 }
 
 function normalizeMatrix(matrix) {
-  let average0 = (matrix[0][0] + matrix[1][0] + matrix[2][0]) / 3;
-  let average1 = (matrix[0][1] + matrix[1][1] + matrix[2][1]) / 3;
-  let average2 = (matrix[0][2] + matrix[1][2] + matrix[2][2]) / 3;
+  let average0 = (matrix[0][0] + matrix[1][0] + matrix[2][0]);
+  let average1 = (matrix[0][1] + matrix[1][1] + matrix[2][1]);
+  let average2 = (matrix[0][2] + matrix[1][2] + matrix[2][2]);
 
   matrix[0][0] = matrix[0][0] / average0;
   matrix[1][0] = matrix[1][0] / average0;
@@ -49,21 +50,15 @@ function normalizeMatrix(matrix) {
   matrix[1][1] = matrix[1][1] / average1;
   matrix[2][1] = matrix[2][1] / average1;
 
-  matrix[0][2] = matrix[0][0] / average2;
-  matrix[1][2] = matrix[1][0] / average2;
-  matrix[2][2] = matrix[2][0] / average2;
+  matrix[0][2] = matrix[0][2] / average2;
+  matrix[1][2] = matrix[1][2] / average2;
+  matrix[2][2] = matrix[2][2] / average2;
 
   return matrix;
 }
 
 function calculateWeights(matrix) {
-  const eigen = numeric.eig(matrix);
-
-  const maxEigenvalueIndex = eigen.lambda.x.indexOf(
-    Math.max(...eigen.lambda.x)
-  );
-
-  const maxEigenvector = eigen.E.x[maxEigenvalueIndex];
+  const maxEigenvector = powerMethod(matrix, 1000);
 
   const normalizedEigenvector = normalizeVector(maxEigenvector);
 
@@ -74,15 +69,40 @@ function calculateWeights(matrix) {
   const criteriaWeights = normalizedEigenvector.map(
     (weight) => weight / sumOfWeights
   );
+  // console.log(criteriaWeights)
 
   return criteriaWeights;
 }
 
+function powerMethod(matrix, numIterations) {
+  // Inicjalizacja losowego wektora startowego
+  let vector = [];
+  for (let i = 0; i < matrix.length; i++) {
+      vector.push(Math.random());
+  }
+
+  // Iteracyjne mnożenie macierzy przez wektor
+  for (let iteration = 0; iteration < numIterations; iteration++) {
+      // Mnożenie macierzy przez wektor
+      let result = matrixVectorMultiply(matrix, vector);
+
+      // Normalizacja wyniku
+      let norm = Math.sqrt(result.reduce((sum, val) => sum + val ** 2, 0));
+      vector = result.map(val => val / norm);
+  }
+
+  return vector;
+}
+
+function matrixVectorMultiply(matrix, vector) {
+  return matrix.map(row => row.reduce((sum, val, index) => sum + val * vector[index], 0));
+}
+
 function normalizeVector(vector) {
-    const sumOfSquares = vector.reduce((sum, element) => sum + element ** 2, 0);
-    const squareRootSum = Math.sqrt(sumOfSquares);
-    const normalizedVector = vector.map(element => Math.abs(element / squareRootSum));
-    return normalizedVector
+  const sumOfSquares = vector.reduce((sum, element) => sum + element ** 2, 0);
+  const squareRootSum = Math.sqrt(sumOfSquares);
+  const normalizedVector = vector.map(element => Math.abs(element / squareRootSum));
+  return normalizedVector
 }
 
 function convertPrice(originalPrice) {
